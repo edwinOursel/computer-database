32c14e1f20ba5416689b860f28b06a26ae5ca059
edwinOursel <eoursel@excilys.com>
2015-03-18 16:03:40 +0100

    test(crud computers)

--- src/test/java/com/excilys/cdb/service/ComputerServiceTest.java
+++ src/test/java/com/excilys/cdb/service/ComputerServiceTest.java
@@ -1,10 +1,17 @@
 package com.excilys.cdb.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.excilys.cdb.exception.ServiceException;
+import com.excilys.cdb.model.Company;
 import com.excilys.cdb.model.Computer;
 
 public class ComputerServiceTest {
@@ -98,15 +105,43 @@ public class ComputerServiceTest {
 			assertTrue(true);
 		}
 	}
-	/*
+	
 	@Test
 	public void testComputerCrud() {
 		//GIVEN
 		ComputerService c = ComputerService.INSTANCE;
+		Computer computer = new Computer();
+		Company company = new Company();
+		company.setName("Edwin corp");
+		long idComputer = 700000000;
+		company.setId(idComputer);
+		computer.setCompany(company);
+		long idCompany = 123456789;
+		computer.setId(idCompany);
+		LocalDateTime t = LocalDateTime.now();
+		computer.setIntroduced(t);
+				
+		try {
+			c.create(computer);
+		} catch (ServiceException e) {
+			fail("create : should not throw an exception");
+		}
 		
 		try {
-			c.create(new Computer());
+			computer = c.getById(idComputer);
+			Assert.assertEquals(idComputer, computer.getId());
+			Assert.assertEquals(computer.getCompany().getId(), idCompany);
+			Assert.assertEquals(computer.getIntroduced(), t);
+		} catch (ServiceException e) {
+			fail("get by id : should not throw an exception");
 		}
-	}*/
+		
+		try {
+			c.delete(idComputer);
+		} catch (ServiceException e) {
+			fail("delete : should not throw an exception");
+		}
+		
+	}
 
 }
