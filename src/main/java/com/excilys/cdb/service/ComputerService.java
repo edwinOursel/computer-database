bfa43063654d467cd03d45c5dfe579aaa3ad4995
edwinOursel <eoursel@excilys.com>
2015-04-16 16:30:46 +0200

    refacto(clean computer service)

--- src/main/java/com/excilys/cdb/service/ComputerService.java
+++ src/main/java/com/excilys/cdb/service/ComputerService.java
@@ -1,27 +1,22 @@
 package com.excilys.cdb.service;
 
 import java.util.List;
-import org.springframework.data.domain.Page;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import com.excilys.cdb.exception.DAOException;
 import com.excilys.cdb.exception.ServiceException;
 import com.excilys.cdb.model.Computer;
-import com.excilys.cdb.persistence.ComputerDatabaseConnectionFactory;
 import com.excilys.cdb.persistence.dao.ComputerRepository;
 
 @Component
 @Transactional(rollbackFor=DAOException.class)
 public class ComputerService extends Service {
-	
-	@Autowired
-	private static ComputerDatabaseConnectionFactory cdcf;
-	
+		
 	@Autowired
 	private ComputerRepository repository;
 			   
@@ -31,65 +26,41 @@ public class ComputerService extends Service {
     }
 	
 	public List<Computer> getAll() throws ServiceException {
-		try {
-			return repository.findAll();
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
+		return repository.findAll();
 	}
 	
 	public Page<Computer> getAll(Pageable page) throws ServiceException {
 		if (page == null) {
 			throw new IllegalArgumentException();
 		}
-		try {
-			return repository.findAll(page);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
+		return repository.findAll(page);
 	}
 
 	public Computer getById(long id) throws ServiceException {
 		if (id <= 0) {
 			throw new IllegalArgumentException();
 		}
-		try {
-			return repository.findById(id);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
+		return repository.findById(id);
 	}
 
 	public long create(Computer computer) throws ServiceException {
 		if (computer == null) {
 			throw new IllegalArgumentException();
 		}
-		try {
-			return repository.save(computer).getId();
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
+		return repository.save(computer).getId();
 	}
 
 	public void update(Computer computer) throws ServiceException {
 		if (computer == null) {
 			throw new IllegalArgumentException();
 		}
-		try {
-			repository.save(computer);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
+		repository.save(computer);
 	}
 
 	public void delete(long id) throws ServiceException {
 		if (id <= 0) {
 			throw new IllegalArgumentException();
 		}
-		try {
-			repository.delete(id);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
+		repository.delete(id);
 	}
 }
